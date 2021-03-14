8ce39129f99f76826906fdf45abb250bdd8aebee
Ben Lipton <ben.lipton@cherre.com>
2021-03-14 14:38:40 -0400

    Fix widget style and delay

--- plugin-opts.zsh
+++ plugin-opts.zsh
@@ -1,3 +1,4 @@
 # Options for marlonrichert/zsh-autocomplete
 zstyle ':autocomplete:tab:*' insert-unambiguous yes
-zstyle ':autocomplete:tab:*' widget-style menu-complete
+zstyle ':autocomplete:tab:*' widget-style menu-select
+zstyle ':autocomplete:*' min-delay 0.2
